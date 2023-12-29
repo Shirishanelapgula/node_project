ad708c19b4da575f9746961b67ea8d5385d38444
shirishanelapogula <shirisha@godspeed.systems>
2023-12-30 02:44:30 +0530

    second commit

--- middleware/errorMiddleWare.js
+++ middleware/errorMiddleWare.js
@@ -1,8 +1,8 @@
 
-const errorMiddleware = (err, req, res, next)=>{
+const errorMiddleWare = (err, req, res, next)=>{
     console.log("this error middleware")
     res.status(err.statusCode ? err.statusCode : 500).json({message: err.message , stack: process.env.NODE_ENV === "development" ? err.stack : null })
 
 }
 
-module.exports = errorMiddleware
+module.exports = errorMiddleWare
