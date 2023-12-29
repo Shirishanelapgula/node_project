7d2f3c84296eb062518a8682c55ad89cb28e1ecb
shirishanelapogula <shirisha@godspeed.systems>
2023-12-30 03:01:01 +0530

    fourth commit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const { getProducts, postProducts, putProducts, deleteProducts , getProductsById} = require('/home/godspeed/node_project/controler/productControler')
+const { getProducts, postProducts, putProducts, deleteProducts , getProductsById} = require('../controler/productControler')
 
 
 
