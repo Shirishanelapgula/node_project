88fab6dec30df848b25b143a975f37a02d55bdb1
shirishanelapogula <shirisha@godspeed.systems>
2023-12-30 17:50:40 +0530

    fifth commit

--- src/index.js
+++ src/index.js
@@ -1,46 +1,32 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('../routes/productRoutes')
 const app = express();
-const PORT = process.env.PORT || 3000
-var cors = require('cors');
-
+const cors = require('cors');
 const  errorMiddleware = require('../middleware/errorMiddleware')
+
+const MONGO_URL = process.env.MONGO_URL
+const FRONTEND = process.env.FRONTEND
+const PORT = process.env.PORT || 3000
+
+const corsOptions = {
+    origin: FRONTEND ,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-const mongoose = require('mongoose');
-
-const productRoutes = require('../routes/productRoutes')
-
-const MONGO_URL = process.env.MONGO_URL
-
+app.use(cors(corsOptions))
 
 app.use('/api/products',productRoutes)
 
 app.use(errorMiddleware)
 
-const FRONTEND=process.env.FRONTEND
-
-var corsOptions = {
-    origin: FRONTEND ,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
-app.use(cors(corsOptions))
-
-// Define a route that responds with "Hello, World!"
 app.get('/', (req, res) => {
     throw new Error('custom error')
-
-
-//   res.send('Hello, shirisha!');
 });
-
-// Start the Express server
-
-
-
 
 
 mongoose.connect(MONGO_URL)
